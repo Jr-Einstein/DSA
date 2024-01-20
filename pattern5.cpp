03416fce9205bf714f2031fea010b9d64402d741
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-01-20 21:40:08 +0530

    Lecture-4

--- pattern5.cpp
+++ pattern5.cpp
@@ -5,11 +5,10 @@ int main(){
     int n;
     cin>>n;
     int i=1;
-    int count =1;
     while(i<=n){
         int j=1;
         while(j<=i){
-            cout<<i;
+            cout<<i-j+1;
             j=j+1;
         }
         cout<<endl;
