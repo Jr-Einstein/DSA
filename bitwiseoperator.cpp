fe60b61b446726d8f056864c598c0a64076716fd
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-01-22 01:20:13 +0530

    Lecture-5

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<(17>>1)<<endl;
+    cout<<(17>>2)<<endl;
+    cout<<(19<<1)<<endl;
+    cout<<(21<<2)<<endl;
+
+
+int i=7;
+cout<<(++i)<<endl;
+cout<<(i++)<<endl;
+cout<<(i--)<<endl;
+cout<<(--i)<<endl;
+
+
+int a,b =1;
+a =10;
+if(++a)
+cout<<b;
+else
+cout<<++b;
+
+}
+    
