6d11ab05569acd28aba55e82991946d4b2fb7a6e
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-03-02 17:35:28 +0530

    Create Simple.java

---
+++ OOPS with C++/Simple.java
@@ -0,0 +1,31 @@
+
+
+class Student{
+    String name;
+    int age;
+
+    public void printinfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+}
+
+
+
+public class simple{
+    public static void main(String[] args) {
+        
+        Student S1 = new Student();
+        S1.name="Aman";
+        S1.age=20;
+
+
+        S1.printinfo();
+        
+
+
+
+    }
+    
+    }
